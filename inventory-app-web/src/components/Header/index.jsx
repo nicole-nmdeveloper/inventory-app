f9a78530d0811fe40e9fcf8038ca951014452d6d
Nicole <102914172+nicole-nmdeveloper@users.noreply.github.com>
2023-11-24 20:06:57 -0300

    Sign Out

--- inventory-app-web/src/components/Header/index.jsx
+++ inventory-app-web/src/components/Header/index.jsx
@@ -1,20 +1,60 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { Nav, Divider } from './styled'
 
+import * as actions from '../../store/modules/auth/actions'
+
+import history from '../../services/history'
+
 export default function Header() {
+  const dispatch = useDispatch()
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+  const userName = useSelector((state) => state.auth.user.name)
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+
+    dispatch(actions.loginFailure())
+
+    history.push('/login')
+  }
+
   return (
     <Nav>
-      <Link to="/login">
+      <Link to="/">
         <span>Home</span>
       </Link>
 
-      <Divider>|</Divider>
+      {isLoggedIn ? (
+        <>
+          <Divider>|</Divider>
 
-      <Link to="/register">
-        <span>Sign up</span>
-      </Link>
+          <Link onClick={handleLogout} to="/logout">
+            <span>Sign out</span>
+          </Link>
+
+          <Divider>|</Divider>
+
+          <span>Welcome, {userName}</span>
+        </>
+      ) : (
+        <>
+          <Divider>|</Divider>
+
+          <Link to="/register">
+            <span>Sign up</span>
+          </Link>
+
+          <Divider>|</Divider>
+
+          <Link to="/login">
+            <span>Sign in</span>
+          </Link>
+        </>
+      )}
     </Nav>
   )
 }
