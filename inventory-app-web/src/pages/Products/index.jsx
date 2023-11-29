f9a43d7c04d926eece2263b468513331b4331d11
Nicole <102914172+nicole-nmdeveloper@users.noreply.github.com>
2023-11-28 21:08:55 -0300

    Product list

--- inventory-app-web/src/pages/Products/index.jsx
+++ inventory-app-web/src/pages/Products/index.jsx
@@ -1,17 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { get } from 'lodash'
+import { Image, Pencil, Trash2Icon } from 'lucide-react'
+import { toast } from 'react-hot-toast'
 
 import { Container } from '../../styles/GlobalStyles'
+import { Table, ProfilePicture } from './styled'
+
+import axios from '../../services/axios'
 
 import Loading from '../../components/Loading'
 
 export default function Products() {
   const [isLoading, setIsLoading] = useState(false)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      setIsLoading(true)
+
+      try {
+        const response = await axios.get('/products/')
+        setProducts(response.data)
+      } catch (err) {
+        toast.error('Ops! Algo deu errado. Tente novamente mais tarde.')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    getData()
+  }, [])
 
   return (
     <Container>
       <Loading isLoading={isLoading} />
 
-      <h1>Products</h1>
+      <h1>Produtos</h1>
+
+      <Table>
+        {products.map((product) => (
+          <div key={String(product.id)}>
+            <ProfilePicture>
+              {get(product, 'Pictures[0].url', false) ? (
+                <img
+                  crossOrigin=""
+                  src={product.Pictures[0].url}
+                  alt="Profile"
+                />
+              ) : (
+                <Image size={48} strokeWidth={1} />
+              )}
+            </ProfilePicture>
+            <span>{product.title}</span>
+            <span>{product.price}</span>
+            <span>{product.amount}</span>
+            <span>{product.variants}</span>
+
+            <Link to={`/product/${product.id}/edit`}>
+              <Pencil size={16} />
+            </Link>
+            <Link to={`/product/${product.id}/delete`}>
+              <Trash2Icon size={16} />
+            </Link>
+          </div>
+        ))}
+      </Table>
     </Container>
   )
 }
--- inventory-app-web/src/pages/Products/styled.js
+++ inventory-app-web/src/pages/Products/styled.js
@@ -1,1 +1,43 @@
 import styled from 'styled-components'
+
+import * as colors from '../../config/colors'
+
+export const Table = styled.div`
+  margin-top: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: 1px solid ${colors.primaryLightColor};
+  }
+
+  a {
+    color: ${colors.primaryColor};
+
+    &:hover {
+      color: ${colors.primaryDarkColor};
+    }
+
+    &:active {
+      color: ${colors.primaryLightColor};
+    }
+  }
+`
+
+export const ProfilePicture = styled.div`
+  width: 35px;
+  height: 45px;
+
+  img {
+    width: 100%;
+    height: 100%;
+
+    border: 2px solid ${colors.darkColor};
+    border-radius: 5px;
+  }
+`
