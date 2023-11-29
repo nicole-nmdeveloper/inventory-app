22b8d9ac4745935dde5002400987a22107682799
Nicole <102914172+nicole-nmdeveloper@users.noreply.github.com>
2023-11-28 21:57:20 -0300

    Deleting a product

--- inventory-app-web/src/pages/Products/index.jsx
+++ inventory-app-web/src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { get } from 'lodash'
-import { Image, Pencil, Trash2Icon } from 'lucide-react'
+import { Image, Pencil, Trash2Icon, AlertCircle } from 'lucide-react'
 import { toast } from 'react-hot-toast'
 
 import { Container } from '../../styles/GlobalStyles'
@@ -32,6 +32,43 @@ export default function Products() {
     getData()
   }, [])
 
+  function handleDeleteAsk(e) {
+    e.preventDefault()
+
+    const exclamation = e.currentTarget.nextSibling
+    exclamation.setAttribute('display', 'block')
+
+    e.currentTarget.remove()
+  }
+
+  async function handleDelete(e, id, index) {
+    e.persist()
+
+    try {
+      setIsLoading(true)
+
+      await axios.delete(`/products/${id}`)
+
+      const newProducts = [...products]
+      newProducts.splice(index, 1)
+      setProducts(newProducts)
+
+      toast.success('Produto excluído.')
+
+      setIsLoading(false)
+    } catch (err) {
+      const status = get(err, 'response.status', 0)
+
+      if (status === 401) {
+        toast.error('Você precisa fazer login.')
+      } else {
+        toast.error('Ocorreu um erro ao excluir aluno.')
+      }
+
+      setIsLoading(false)
+    }
+  }
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
@@ -39,17 +76,17 @@ export default function Products() {
       <h1>Produtos</h1>
 
       <Table>
-        {products.map((product) => (
+        {products.map((product, index) => (
           <div key={String(product.id)}>
             <ProfilePicture>
               {get(product, 'Pictures[0].url', false) ? (
                 <img
                   crossOrigin=""
                   src={product.Pictures[0].url}
-                  alt="Profile"
+                  alt="Product"
                 />
               ) : (
-                <Image size={48} strokeWidth={1} />
+                <Image size={48} strokeWidth={1} alt="Product" />
               )}
             </ProfilePicture>
             <span>{product.title}</span>
@@ -60,9 +97,21 @@ export default function Products() {
             <Link to={`/product/${product.id}/edit`}>
               <Pencil size={16} />
             </Link>
-            <Link to={`/product/${product.id}/delete`}>
+
+            <Link
+              onClick={handleDeleteAsk}
+              to={`/product/${product.id}/delete`}
+            >
               <Trash2Icon size={16} />
             </Link>
+
+            <AlertCircle
+              size={16}
+              display="none"
+              cursor="pointer"
+              color="#B22222"
+              onClick={(e) => handleDelete(e, product.id, index)}
+            />
           </div>
         ))}
       </Table>
